61d11ab2178a2bcd39359ccd7badd07b8e9d6364
Michael Zuercher <michzuerch@gmail.com>
2024-09-27 10:12:00 +0200

    add webdevicons

--- config/default.nix
+++ config/default.nix
@@ -46,7 +46,7 @@
     #./plug/ui/precognition.nix
     ./plug/ui/smart-splits.nix
     ./plug/ui/telescope.nix
-    ./plug/ui/tagbar.nix
+    ./plug/ui/web-devicons.nix
 
     ./plug/utils/colorizer.nix
     ./plug/utils/comment-box.nix
